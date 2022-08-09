1cfdeca6f3cc43ffdcbcd62979911b8c45525c35
mug3njutsu <johngitahi272@gmail.com>
2022-08-09 16:17:45 +0300

    print_rot13.c

---
+++ print_rot13.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_rot13 - encodes a string to rot13.
+  * @R: str to convert
+  * Return: sizeof(stdout)
+  */
+
+int print_rot13(va_list R)
+{
+	int j, i, count = 0;
+	char *r;
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLM nopqrstuvwxyzabcdefghijklm";
+
+	r = va_arg(R, char *);
+	if (r == NULL)
+		r = "(null)";
+	for (j = 0; r[j] != '\0'; j++)
+	{
+		for (i = 0; input[i] != '\0'; i++)
+		{
+			if (r[j] == input[i])
+			{
+				_putchar(output[i]);
+				count++;
+				break;
+			}
+		}
+	}
+	return (count);
+}
