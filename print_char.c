f9076d041912bd8c58dd09f6f7c6720fc1ab34d3
mug3njutsu <johngitahi272@gmail.com>
2022-08-09 15:59:50 +0300

    print_char.c

---
+++ print_char.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_char - writes to stdout.
+ * @c: char to print
+ * Return: 1.
+ */
+
+int print_char(va_list c)
+{
+	unsigned char my_char;
+
+	my_char = va_arg(c, int);
+	_putchar(my_char);
+	return (1);
+}
+
+/**
+ * print_percentage - %
+ * Return: 1.
+ */
+int print_porcentage(void)
+{
+	_putchar('%');
+	return (1);
+}
