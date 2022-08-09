69c481ee6f43d3610f3e2999a92197d9ba8344c3
mug3njutsu <johngitahi272@gmail.com>
2022-08-09 16:41:51 +0300

    main.h

--- main.h
+++ main.h
@@ -2,31 +2,36 @@
 #define MAIN_H
 
 #include <stdarg.h>
-int _putchar(char c);
-int _printf(const char *format, ...);
-int print_char(va_list c);
-int print_string(va_list s);
-int print_int(va_list i);
-int print_dec(va_list d);
-int print_rev(va_list r);
-int print_bin(va_list b);
-int print_unsig(va_list u);
-int print_octal(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_rot13(va_list R);
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
-  * struct code_format - Struct format
-  *
-  * @sc: The specifiers
-  * @f: The function associated
-  */
+ * struct structprint - func
+ * @q: location and method to translate data to chars.
+ * @u: print function for specific type.
+ * Return: int
+ */
 
-typedef struct code_format
+typedef struct structprint
 {
-char *sc;
-int (*f)(va_list);
-} code_f;
+char *q;
+int (*u)(char *format, va_list);
+} structype;
+
+int _putchar(char ch);
+int _puts(char *string);
+int printc(char *format, va_list);
+int printstr(char *format, va_list);
+int (*driver(char *format))(char *format, va_list);
+int _printf(char *format, ...);
+int printint(char *format, va_list pa);
+int integer(int number);
+int contadordigit(int number);
+int _abs(int number);
+int printpercent(char *format, va_list pa);
+int printhex(char *format, va_list);
+int printHEX(char *format, va_list);
+int printocta(char *format, va_list);
+int print_unsign(char *format, va_list);
 
 #endif
